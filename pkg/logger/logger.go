7f589c2fdb358af88e6436098bb2590f68a8f994
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-20 16:12:29 +0330

    function to return new zaplogger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,13 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func NewZap(cfg *Config) *zap.Logger {
+	return zap.New(
+		zapcore.NewCore(getEncoder(cfg), getWriteSyncer(cfg), getLoggerLevel(cfg)),
+		getOptions(cfg)...,
+	)
+}
