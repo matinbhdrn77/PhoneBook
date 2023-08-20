084b130a93a3eea005a5d99df84ea8e141df1d01
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-20 15:49:40 +0330

    methods for adding and running commands

--- cmd/server.go
+++ cmd/server.go
@@ -1,1 +1,17 @@
 package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+type Server struct{}
+
+func (cmd Server) Command(trap chan os.Signal) *cobra.Command {
+	return nil
+}
+
+func (cmd *Server) run(cfg *config.Config, trap chan os.Signal) {
+
+}
