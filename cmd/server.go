ca6055567e682cf0f365f2947d745ec6e751492b
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-20 17:47:56 +0330

    log error and catch signal and pass it zap

--- cmd/server.go
+++ cmd/server.go
@@ -4,14 +4,16 @@ import (
 	"os"
 
 	"github.com/matinbhdrn77/PhoneBook/internal/config"
+	"github.com/matinbhdrn77/PhoneBook/pkg/logger"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 type Server struct{}
 
 func (cmd Server) Command(trap chan os.Signal) *cobra.Command {
 	run := func(_ *cobra.Command, _ []string) {
-		cmd.run(&config.Config{}, trap)
+		cmd.run(config.Load(true), trap)
 	}
 
 	return &cobra.Command{
@@ -22,5 +24,10 @@ func (cmd Server) Command(trap chan os.Signal) *cobra.Command {
 }
 
 func (cmd *Server) run(cfg *config.Config, trap chan os.Signal) {
+	logger := logger.NewZap(cfg.Logger)
 
+	logger.Error("Implement me")
+
+	field := zap.String("signal trap", (<-trap).String())
+	logger.Info("exiting by recieving unix signal:", field)
 }
