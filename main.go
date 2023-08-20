07cac2f8aeec00f2d16461e79a98938f6d409bf4
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-20 15:35:25 +0330

    traping int and term signals for graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
 	const description = "PhoneBook Application"
 	root := &cobra.Command{Short: description}
+
+	trap := make(chan os.Signal, 1)
+	signal.Notify(trap, syscall.SIGINT, syscall.SIGTERM)
 
 }
